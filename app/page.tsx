a8e74023baef28ef166fbc3c0f808a924d7ca6ac
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-03-23 20:27:56 +0300

    hn

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Poppins } from "next/font/google";
-const poppins = Poppins({
-  weight: ["400", "500", "600"],
-  subsets: ["latin"], // Ensure Latin characters are loaded
-  variable: "--font-poppins", // Set a CSS variable
-});
 
 export default function Home() {
   return (
