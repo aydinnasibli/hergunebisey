37ee8559f0f65922b2818a1fcec7eece4f5092cb
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-03-29 11:18:38 +0300

    a

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
-
+import Image from 'next/image'
 function blog() {
     return (
         <div>
-            <h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1><h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1>
-            <h1 className='text-right'>bücürotti</h1>
+            <section className="relative flex items-center justify-center h-[650px] bg-gray-200">
+                <Image
+                    src="https://i.imghippo.com/files/NmX4787FXk.jpg" // Replace with actual image path
+                    alt="Hero Background"
+                    fill
+                    className="object-cover"
+                    priority
+                />
+                <div className="absolute text-white text-xl z-10">Overlay Content</div>
+            </section>
             <h1 className='text-right'>bücürotti</h1>
             <h1 className='text-right'>bücürotti</h1>
             <h1 className='text-right'>bücürotti</h1>
