29eb3676f72a8052c87bf2e2d151b64f64c71431
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-04-02 23:17:43 +0300

    a

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,9 +1,5 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import blog from './blog'
-import author from './author'
-import category from './category'
-import comment from './comment'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blog, author, category, comment],
+  types: [],
 }
