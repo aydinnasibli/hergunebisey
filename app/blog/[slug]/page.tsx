6ce9b987ef5e71d3e6aeb8bdf438ce56890589e0
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-04-03 17:18:04 +0300

    blogHeader

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -28,8 +28,7 @@ export default async function BlogPost({ params }: { params: { slug: string } })
             ),
         },
         marks: {
-            link: ({ children, value }: { children: React.ReactNode; value: { href: string } }) => {
-
+            link: ({ children, value }: { children: React.ReactNode, value: any }) => {
                 const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined
                 const target = !value.href.startsWith('/') ? '_blank' : undefined
                 return (
