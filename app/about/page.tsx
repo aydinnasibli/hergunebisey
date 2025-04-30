cc619fa1e03b6c0c77fcdd0dc9ffb488887affc9
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-04-30 04:09:13 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import { throttle } from 'lodash';
 
 const AboutUs = () => {
